8475388240f010c65c9d4135e73881eef1dae13a
Lucas Melz <lucas.melz@mindera.com>
2024-02-13 23:13:20 +0000

    Added scope prefix to classes of each project to prevent conflicts

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => (
-  <div>
-    <h1>Hi there!</h1>
-    <hr />
-    <MarketingApp />
-  </div>
+  <BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </StylesProvider>
+  </BrowserRouter>
 );
