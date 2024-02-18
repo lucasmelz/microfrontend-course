5bbb4b67a76be7574bbb6dd317016a2caebc8171
Lucas Melz <lucas.melz@mindera.com>
2024-02-18 00:39:00 +0000

    Added Auth project pipeline, lazy loading projects

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
-import AuthApp from "./components/AuthApp";
+
 import Header from "./components/Header";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -14,10 +16,12 @@ export default () => (
     <StylesProvider generateClassName={generateClassName}>
       <div>
         <Header />
-        <Switch>
-          <Route path="/auth" component={AuthApp} />
-          <Route path="/" component={MarketingApp} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/auth" component={AuthLazy} />
+            <Route path="/" component={MarketingLazy} />
+          </Switch>
+        </Suspense>
       </div>
     </StylesProvider>
   </BrowserRouter>
